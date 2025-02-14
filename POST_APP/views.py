3af8f69d9e1cafd4adc8feca9559a350e4dc2f8d
sardorxonakramov <sardorxonakramov@gmail.com>
2025-02-14 20:15:33 +0500

    post veb sahifasi uchun CRUD amali qo'shildi va Postning detail pagi ham qo'shildi

--- POST_APP/views.py
+++ POST_APP/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView,UpdateView, DeleteView
 from .models import Post
+from .forms import PostForm
 # Create your views here.
 
 
@@ -9,3 +11,33 @@ class PostLView(ListView):
     template_name = 'POST_APP/home.html'
     context_object_name = 'posts'
     ordering = ['-date_posted']
+
+class DetailPView(DetailView):
+    model = Post
+    pk_url_kwarg = 'post_id'
+    template_name = 'POST_APP/detail.html'
+    context_object_name = 'post'
+    ordering = ['-date_posted']
+
+class CreatePView(CreateView):
+    model = Post
+    template_name = 'POST_APP/create.html'
+    form_class = PostForm
+    def get_success_url(self):
+        return reverse_lazy('detail', kwargs={'post_id': self.object.id})
+    
+class UpdatePView(UpdateView):
+    model = Post
+    template_name = 'POST_APP/update.html'
+    form_class = PostForm
+    pk_url_kwarg = 'post_id'
+    def get_success_url(self):
+        return reverse_lazy('detail', kwargs={'post_id': self.object.id})
+    
+class DeletePView(DeleteView):
+    model = Post
+    template_name = 'POST_APP/delete.html'
+    context_object_name = 'post'
+    pk_url_kwarg = 'post_id'
+    success_url = reverse_lazy('home')
+    
